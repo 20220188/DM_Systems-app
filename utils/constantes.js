5c8505ecdfac75efeb20b46609097887da976c46
Juan2022-a <20220671@ricaldone.edu.sv>
2024-07-23 12:00:29 -0600

    Update constantes.js

--- utils/constantes.js
+++ utils/constantes.js
@@ -4,8 +4,11 @@
 //export const IP = 'http://10.10.0.165';
 //export const IP = 'http://192.168.137.65';
 
+//IP lab de Will JP
+export const IP = 'http://10.10.0.66';
+
 //IP lab de Will Musun
-export const IP = 'http://10.10.2.108';
+//export const IP = 'http://10.10.2.108';
 
 //IP Musun
 //export const IP = 'http://192.168.1.18';
