b72c91d326e372ce315e5136d8b17df4b9775416
20220188 <20220188@ricaldone.edu.sv>
2024-09-16 07:36:30 -0600

    arreglo del nabifation

--- src/screens/navigation.js
+++ src/screens/navigation.js
@@ -1,11 +1,7 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import LoadingScreen from './LoadingScreen';
-import Register from './Register';
 import Login from './Login';
-import Recu from './RecuperacionContraseñas';
-import Codigo from './CodigoRecuperacion';
-import Contra from './CambiarContraseñas';
 import Admins from './Admins';
 import Inicio from './HomeScreen';
 import Puntos from './PuntosVenta';
@@ -20,11 +16,7 @@ function AppNavigator() {
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Loading">
         <Stack.Screen name="Loading" component={LoadingScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="Register" component={Register} options={{ headerShown: false }} />
         <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
-        <Stack.Screen name="RecuperacionContraseñas" component={Recu} options={{ headerShown: false }} />
-        <Stack.Screen name="CodigoRecuperacion" component={Codigo} options={{ headerShown: false }} />
-        <Stack.Screen name="CambiarContraseñas" component={Contra} options={{ headerShown: false }} />
         <Stack.Screen name="Admins" component={Admins} options={{ headerShown: false }} />
         <Stack.Screen name="HomeScreen" component={Inicio} options={{ headerShown: false }} />
         <Stack.Screen name="PuntosVenta" component={Puntos} options={{ headerShown: false }} />
